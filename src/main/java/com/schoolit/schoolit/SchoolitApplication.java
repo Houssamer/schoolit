fe3f85bd95995b76674932e0b04a4dfbe9284ddf
Houssamer <riad.houssame.eleve@ehtp.ac.ma>
2022-03-31 09:55:31 +0000

    init

---
+++ src/main/java/com/schoolit/schoolit/SchoolitApplication.java
@@ -0,0 +1,13 @@
+package com.schoolit.schoolit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolit/schoolit/SchoolitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolit.schoolit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
